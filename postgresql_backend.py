70b4d245bf5cf0f614aa521a224f3fb7400ea238
Rory S Moeller <moellerr@oregonstate.edu>
2021-11-16 10:16:46 -0800

    Initial user stuff

--- postgresql_backend.py
+++ postgresql_backend.py
@@ -1,10 +1,11 @@
 import psycopg2 as db_con  # Manage the connection to databse
 import os  # Fetch credentials from the environment
 from sys import stderr, argv # Print errors to stderr
-from flask import Flask, json  # The framework for backend & dev server
+from flask import Flask, json, request  # The framework for backend & dev server
 from gevent.pywsgi import WSGIServer  # The production server for backend
 from decimal import Decimal  # Allow override for decimal type in json serialization
 import re # Regular expression for validation of input
+import hashlib  # For hashing passwords
 
 
 app = Flask(__name__)  # Create the flask app
@@ -35,19 +36,35 @@ class DatabaseConnection():
         json.JSONEncoder.default = self.default
     def execute_query(self, query, user_input="", include_headers=False):
         # Execute the query and return the results
-        query = self.clean_query(query)
-        self.cursor.execute(query, (user_input,))
+        user_input = [self.clean_query(i) for i in user_input]            
+        self.cursor.execute(query, user_input)
         if include_headers:
             columns = [column[0] for column in self.cursor.description]
             return columns, self.cursor.fetchall()
         return self.cursor.fetchall()
+    def execute_insert(self, query : str, user_input : list):
+        # Execute the query and return the results
+        user_input = [self.clean_query(i) for i in user_input]
+        self.cursor.execute(query, user_input)
+        self.connection.commit()
+    def get_password_hash(self, username):
+        # Get the password hash for a user
+        username = self.clean_query(username)
+        print(f'{username=}')
+        self.cursor.execute("SELECT password_hash FROM rev2.users WHERE user_name = %s", [username])
+        try:
+           return self.cursor.fetchone()[0]
+        except TypeError:
+            return None
     def close_connection(self):
         self.connection.close()
     def clean_query(self, query):
         # Very basic attempt at cleaning up the query
         # I believe psycopg2 does a fair bit of protection
         # but I'm sure it's not 100%
-        return query.replace("'", "''").replace(";", "")
+        if isinstance(query, str):
+            return query.replace("'", "''")
+        return query
     def default(self, o):
         # Override the default json serialization for decimal type
         if isinstance(o, Decimal):
@@ -61,6 +78,10 @@ class BackendRESTAPI():
         self.port_number    = port_num
         self.host           = "localhost"
         self.devenv         = True
+        try:
+            self.pepper = os.environ["PEPPER"]
+        except KeyError:
+            self.pepper = ""
         
         @app.route("/", methods=["GET"])
         def index():
@@ -78,10 +99,83 @@ class BackendRESTAPI():
             header, res = self.db_connection.execute_query(query, user_input, include_headers=True)
             return json.jsonify({"header": header, "results": res})
         
+        @app.route("/u/login", methods=["POST"])
+        def login():
+            # Validate the login
+            re.match(r"\$a-zA-Z0-9_]{1,20}", request.form["username"])
+            re.match(r"\$a-zA-Z0-9_]{1,20}", request.form["password"])
+            un = request.form["username"]
+            pw = request.form["password"]
+            # Execute the query
+            password_hash = self.db_connection.get_password_hash(un)
+            if password_hash is None:
+                return json.jsonify({"error": "Incorrect password or username"})
+            print("nice")
+            try:
+                salt, hashed_password = password_hash.split("$")
+                hash_val = hashlib.sha256((self.pepper + salt + request.form["password"]).encode()).hexdigest()
+                if hash_val == hashed_password:
+                    return json.jsonify({"success": True})
+            except:
+                pass
+            return json.jsonify({"success": False})
+
+        @app.route("/u/register", methods=["POST"])
+        def register():
+            try:
+                # Validate the registration
+                if not re.fullmatch(r"([a-zA-Z0-9_]{1,20}$)", request.form["username"]):
+                    return json.jsonify({"error": "Invalid char or length in username"})
+                if not re.fullmatch(r"([a-zA-Z0-9_]{1,20}$)", request.form["password"]):
+                    return json.jsonify({"error": "Invalid char or length in password"})
+                if not re.fullmatch( r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", request.form["email"]):
+                    return json.jsonify({"error": "Bad email"})
+
+                """everything below this is super gross
+                please make sure to include these fields in your form
+                and post requests so i can delete this """
+                try:
+                    request.form["name"]
+                except:
+                    return json.jsonify({"error": "No name provided"})
+                try:
+                    bio = request.form["bio"]
+                except:
+                    bio = ""
+                try:
+                    phone = request.form["phone_number"]
+                except:
+                    phone = "0"
+                try:
+                    website = request.form["website"]
+                except:
+                    website = ""
+                try:
+                    role_type = request.form["user_role_type"]
+                except:
+                    role_type = "0"
+                
+                # Create the salt
+                salt = os.urandom(16).hex()
+                # Hash the password
+                hash_val = hashlib.sha256((self.pepper + salt + request.form["password"]).encode()).hexdigest()
+                password_hash = salt + "$" + hash_val
+                # Execute the query
+                try:
+                    self.db_connection.execute_insert("INSERT INTO rev2.users\
+                        (user_name,                bio, email,                 phone_number, website, name,                 user_role_type, password_hash) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)",
+                        (request.form["username"], bio, request.form["email"], phone,        website, request.form["name"], role_type,      password_hash))
+                    return json.jsonify({"success": True})
+                except db_con.errors.UniqueViolation:
+                    return json.jsonify({"error": "Username already exists"})
+            except KeyError:
+                return json.jsonify({"success": False})
+
+
         # Start the server
         if self.devenv:
             # Development server
-            app.run(host=self.host, port=self.port_number)
+            app.run(host=self.host, port=self.port_number, debug=True, use_reloader=True)
         else:
             # Production server
             http_server = WSGIServer((self.host, self.port_number), app)
@@ -91,9 +185,9 @@ class BackendRESTAPI():
 def main():
     # May change argv passing to env variables
     if len(argv) > 1:
-        api = BackendRESTAPI(int(argv[1]))
+        BackendRESTAPI(int(argv[1]))
     else:
-        api = BackendRESTAPI()
+        BackendRESTAPI()
 
 
 if __name__ == "__main__":
