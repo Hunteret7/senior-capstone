aabb2d9b7ad59044ddc8dc677fc7da7c268c082f
Rory S Moeller <moellerr@oregonstate.edu>
2022-01-06 17:48:16 -0800

    New server.js approach

--- Server.js
+++ Server.js
@@ -1,8 +1,23 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
 const express = require('express');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/src/app'));
-// Start the app by listening on the default
-// Heroku port
+const app = express();
+app.use(requireHTTPS);
+
+app.use(express.static('. / dist / < name - on - package.json > '));
+
+app.get(' /*', function(req, res) {
+    res.sendFile('index.html', {
+        root: 'dist/<name-on-package.json>/'}
+    );
+});
+
 app.listen(process.env.PORT || 8080);
+
