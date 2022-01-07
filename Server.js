d463e5b388da66751668c5107a6da428460762f7
Rory S Moeller <moellerr@oregonstate.edu>
2022-01-06 17:51:44 -0800

    I HATE MYSELF

--- Server.js
+++ Server.js
@@ -8,7 +8,6 @@ function requireHTTPS(req, res, next) {
 
 const express = require('express');
 const app = express();
-const app = express();
 app.use(requireHTTPS);
 
 app.use(express.static('. / dist / < name - on - package.json > '));
