2aeb174fbfc15f297b18153410279fb49901a63c
Hunteret7 <56205404+Hunteret7@users.noreply.github.com>
2022-01-06 17:30:54 -0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { FormsModule } from '@angular/forms'; // NgModel lives here
 import { AppComponent } from './app.component';
