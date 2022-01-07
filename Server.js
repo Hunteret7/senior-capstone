9118545989afce80c8c4becdbd35cddc0d61404b
Rory S Moeller <moellerr@oregonstate.edu>
2022-01-06 18:20:07 -0800

    require https

--- Server.js
+++ Server.js
@@ -8,7 +8,7 @@ function requireHTTPS(req, res, next) {
 
 const express = require('express');
 const app = express();
-// app.use(requireHTTPS);
+app.use(requireHTTPS);
 
 app.use(express.static('./dist/my-app'));
 
