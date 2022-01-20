34017a02ee45a289b956caf17de27bc6b330cf06
kennyzuo <353715843@qq.com>
2022-01-20 14:45:12 +0800

    demo commit

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core"
 import * as L from "leaflet"
+
 @Component({
   selector: "app-map",
   templateUrl: "./map.component.html",
@@ -8,18 +9,17 @@ import * as L from "leaflet"
 export class MapComponent implements OnInit {
   constructor() {}
 
+  map: any
+
   ngOnInit(): void {
+    this.map = L.map("map").setView([51.505, -0.09], 13)
     this.initMap()
   }
 
   initMap(): void {
-    let map = L.map("map").setView([51.505, -0.09], 13)
-
     L.tileLayer(
       "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
       {
-        attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         id: "mapbox/streets-v11",
         tileSize: 512,
@@ -27,6 +27,26 @@ export class MapComponent implements OnInit {
         accessToken:
           "pk.eyJ1Ijoiemhlbmd6dW8iLCJhIjoiY2t5bWM5cTk5M2YwbjJ2cGI4cmpwZmhkNCJ9.IzkLw4whcSBVtLpqiE3NYg",
       }
-    ).addTo(map)
+    ).addTo(this.map)
+  }
+
+  changeMap(type: String): void {
+    let id = "mapbox/streets-v11"
+    let url = "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}"
+    if (type == "2") {
+      id = "mapbox/dark-v10"
+    }
+    console.log(id)
+    L.tileLayer(
+      url,
+      {
+        maxZoom: 18,
+        id,
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken:
+          "pk.eyJ1Ijoiemhlbmd6dW8iLCJhIjoiY2t5bWM5cTk5M2YwbjJ2cGI4cmpwZmhkNCJ9.IzkLw4whcSBVtLpqiE3NYg",
+      }
+    ).addTo(this.map)
   }
 }
