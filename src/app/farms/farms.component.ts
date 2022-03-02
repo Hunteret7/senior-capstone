fbd66f349d632723225fb969575583a734fd613a
Rory S Moeller <moellerr@oregonstate.edu>
2022-03-01 16:33:55 -0800

    This is a 4-space indentation repo. +Fixed .post()

--- src/app/farms/farms.component.ts
+++ src/app/farms/farms.component.ts
@@ -2,48 +2,58 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { stringify } from '@angular/compiler/src/util';
 @Component({
-  selector: 'app-farms',
-  templateUrl: './farms.component.html',
-  styleUrls: ['./farms.component.css']
+    selector: 'app-farms',
+    templateUrl: './farms.component.html',
+    styleUrls: ['./farms.component.css']
 })
 
 export class FarmsComponent implements OnInit {
-//  constructor() {}
-//  seedlots = SEEDLOTS;
-  li:any;
-  object:any;
-  headers:any[] = [];
-  values:any[] = [];
-  
-//  constructor(private seedlotService: SeedlotService) {}
+    //  constructor() {}
+    //  seedlots = SEEDLOTS;
+    li: any;
+    object: any;
+    headers: any[] = [];
+    values: any[] = [];
 
-constructor(private http : HttpClient){
-}
+    //  constructor(private seedlotService: SeedlotService) {}
 
-//  ngOnInit(): void{}
-  
-//  getSeedlots(): void {
-//    this.seedlotService.getSeedlots()
-//    	.subscribe(seedlots => this.seedlots = seedlots);
-//  }
+    constructor(private http: HttpClient) {
+    }
 
-//  ngOnInit(): void {
-//    this.getSeedlots();
-//  }
-  ngOnInit(): void {
-//  this.http.get('http://native-plants-backend.herokuapp.com/')
-  this.http.get('https://native-plants-backend.herokuapp.com/q/SELECT farm_name, farm_location, contact_email FROM rev2.farms')
-  .subscribe(Response => {
-  
-  // If response comes hideloader() function is called
-  // to hide that loader
-  this.object = JSON.parse(JSON.stringify(Response))
-  // console.log("obj:", this.object)
-  this.headers=this.object.headers;
-  this.values = this.object.data
-  });
-}
-saveProduct() {
-  this.http.post('https://native-plants-backend.herokuapp.com/i/INSERT INTO rev2.farms(farm_name) VALUES (%s) / hunteret farmss',JSON.stringify({}))
-}
+    //  ngOnInit(): void{}
+
+    //  getSeedlots(): void {
+    //    this.seedlotService.getSeedlots()
+    //    	.subscribe(seedlots => this.seedlots = seedlots);
+    //  }
+
+    //  ngOnInit(): void {
+    //    this.getSeedlots();
+    //  }
+    ngOnInit(): void {
+        //  this.http.get('http://native-plants-backend.herokuapp.com/')
+        this.http.get('https://native-plants-backend.herokuapp.com/q/SELECT farm_name, farm_location, contact_email FROM rev2.farms')
+            .subscribe(Response => {
+
+                // If response comes hideloader() function is called
+                // to hide that loader
+                this.object = JSON.parse(JSON.stringify(Response))
+                // console.log("obj:", this.object)
+                this.headers = this.object.headers;
+                this.values = this.object.data
+            });
+    }
+    saveProduct() {
+        console.log("hello")
+        this.http.post('https://native-plants-backend.herokuapp.com/i/INSERT INTO rev2.farms(farm_name) VALUES (%s) / hunteret farmss', '{}')
+            .subscribe(Response => {
+                let res = JSON.parse(JSON.stringify(Response))
+                if (res.success) {
+                    alert("Successfully added the farm to the database")
+                } else {
+                    alert("Adding the farm failed. The console may have more details.")
+                    console.log("Failed to push object to the database.\n==Response\n", res)
+                }
+            });
+    }
 }
